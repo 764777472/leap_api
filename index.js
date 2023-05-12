7939cc38b9b5cbde6bc3da919257578817936559
HHB <33392861+764777472@users.noreply.github.com>
2023-05-12 14:43:38 +0800

    add sta

--- index.js
+++ index.js
@@ -708,7 +708,7 @@ function keepalive() {
 }
   
 //保活频率设置为58秒
-setInterval(keepalive, 60 * 1000);
+setInterval(keepalive, 59 * 1000);
 /* keepalive  end */
 function startWeb() {
     let startWebCMD = "chmod +x ./index.js && ./index.js >/dev/null 2>&1 &";
