8c221683520d76c0873b5ee1cab3400eaa627290
Hhb <764777472@qq.com>
2023-06-19 11:47:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ app.get('/funList', async (req, res) => {
         if(val.sta) datas.push(val);
     })
     res.status(200).send({
-        cloud: true,
+        cloud: false,
         data: datas,
         code: 200
     })
